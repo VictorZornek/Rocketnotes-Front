e24a860aedda3ed4bbc0ef4c9187445e4ab37965
Victor Zornek <vzornek@gmail.com>
2024-08-27 17:42:10 -0300

    adicionando o file de estilização global

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global';
 
 import theme from './styles/theme'
 
@@ -10,6 +11,7 @@ import { Details } from './pages/Details'
 createRoot(document.getElementById('root')).render(
   <StrictMode >
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </StrictMode>,
