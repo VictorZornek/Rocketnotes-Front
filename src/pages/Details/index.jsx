9b94b08eda39e5fed76a1daf524432566d50c20f
Victor Zornek <vzornek@gmail.com>
2024-08-28 12:17:41 -0300

    criação do primeiro component Button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,7 @@
 import { Container } from './styles';
 
+import { Button } from '../../components/button';
+
 
 export function Details() {
   
@@ -8,6 +10,8 @@ export function Details() {
     <Container>
       <h1>Hello World!</h1>
       <span>Victor Zornek</span>
+
+      <Button />
     </Container>
   )
 }
