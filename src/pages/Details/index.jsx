e7239bda5cba093c3c8f756c212d72c01b04e5fc
Victor Zornek <vzornek@gmail.com>
2024-08-28 17:13:50 -0300

    criação do componente ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,10 @@
 import { Container, Links } from './styles';
 
-import { Header } from '../../components/header';
-import { Button } from '../../components/button';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
 
 
 export function Details() {
@@ -12,6 +13,8 @@ export function Details() {
   return(
     <Container>
       <Header />
+
+      <ButtonText title="Excluir nota" />
 
       <Section title="Links úteis">
         <Links>
