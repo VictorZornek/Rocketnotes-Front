11e016b00de6879677084c22f0a8452ae9682023
Victor Zornek <vzornek@gmail.com>
2024-08-28 14:54:32 -0300

    finalização do componente Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
     return (
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/VictorZornek.png" alt="Foto do usuário" />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Victor Zornek</strong>
+                </div>
+            </Profile>
         </Container>
     )
 }
