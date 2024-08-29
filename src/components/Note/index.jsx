aaec79b798842ebef164e06f331f28fa16ed431c
Victor Zornek <vzornek@gmail.com>
2024-08-29 18:03:08 -0300

    adicionando a estrutura da page SignIn

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -11,7 +11,7 @@ export function Note({ data, ...rest }) {
                 data.tags &&
                 <footer>
                     {
-                        data.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
+                        data.tags.map(tag => <Tag key={tag.id} title={tag.name} />)    // deve ser utilizado uma key por gerar várias tags
                     }
                 </footer>
             }
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import GlobalStyles from './styles/global';
 
 import theme from './styles/theme'
 
-import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </StrictMode>,
 )
