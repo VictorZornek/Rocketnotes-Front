aad3e4180286e1b2ba762b2bfb97504e4f57e784
Victor Zornek <vzornek@gmail.com>
2024-09-27 17:18:49 -0300

    adicionando a atualização de perfil do usuário

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -10,12 +10,24 @@ import { Button } from "../../components/Button";
 import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
-    const { user } = useAuth();
+    const { user, updateProfile } = useAuth();
 
     const [name, setName] = useState(user.name);
     const [email, setEmail] = useState(user.email);
     const [passwordOld, setPasswordOld] = useState();
     const [passwordNew, setPasswordNew] = useState();
+
+    async function handleUpdate() {
+
+        const user = {
+            name,
+            email,
+            password: passwordNew,
+            old_password: passwordOld
+        }
+
+        await updateProfile({ user })
+    }
 
     return(
         <Container>
@@ -59,15 +71,17 @@ export function Profile() {
                     placeholder='Senha atual'
                     type='password'
                     icon={FiLock}
+                    onChange={e => setPasswordOld(e.target.value)}
                 />
 
                 <Input 
                     placeholder='Nova senha'
                     type='password'
                     icon={FiLock}
+                    onChange={e => setPasswordNew(e.target.value)}
                 />
 
-                <Button title='Salvar' />
+                <Button title='Salvar' onClick={handleUpdate} />
 
             </Form>
         </Container>
