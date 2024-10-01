5056130e42b049ba0d7a25623044002d7b9a05f7
Victor Zornek <vzornek@gmail.com>
2024-10-01 16:17:25 -0300

    adicionando o carregando de notas na Home e filtragem por tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,8 +12,10 @@ import { Note } from '../../components/Note';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Home() {
+    const [search, setSearch] = useState("")
     const [tags, setTags] = useState([]);
     const [tagsSelected, setTagsSelected] = useState([])
+    const [notes, setNotes] = useState([])
 
     function handleTagSelected(tagName) {
         const alreadySelected = tagsSelected.includes(tagName)
@@ -38,6 +40,16 @@ export function Home() {
         fetchTags()
 
     }, [])
+
+    useEffect(() => {
+        async function fetchNotes() {
+            const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`)
+            setNotes(response.data)
+        }
+
+        fetchNotes()
+
+    }, [tagsSelected, search])
 
 
     return (
@@ -70,19 +82,22 @@ export function Home() {
             </Menu>
 
             <Search>
-                <Input placeholder="Pesquisar pelo título" />
+                <Input
+                    placeholder="Pesquisar pelo título"
+                    onChange={() => setSearch(e.target.value)}
+                />
             </Search>
 
             <Content>
                 <Section title='Minhas notas'>
-                    <Note data={{ 
-                        title: 'React', 
-                        tags: [
-                            {id: '1', name: 'react'},
-                            {id: '2', name: 'rocketseat'},
-                        ]
-                    }}
-                    />
+                    {
+                        notes.map(note => (
+                            <Note
+                                key={String(note.id)}
+                                data={note}
+                            />
+                        ))
+                    }
                 </Section>
             </Content>
 
